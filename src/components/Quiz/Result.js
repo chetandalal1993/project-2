2a4535e849dac918ae2cabc6e61d8ab4fc9dac96
YardenLawson <yardenlawson@gmail.com>
2020-05-03 18:43:09 +0100

    update

--- src/components/Quiz/Result.js
+++ src/components/Quiz/Result.js
@@ -10,7 +10,7 @@ class Result extends React.Component {
   state =  {
     finalImage: [
       {name: "Gryffindor", img: Gryffindor, info: "You belong in Gryffindor,\nWhere dwell the brave at heart,\nTheir daring, nerve, and chivalry\n Set Gryffindors apart."},
-      {name: "Slytherin", img: Slytherin, info: "You belong in Slytherin\nYou'll make your real friends,\nThose cunning folks use any means\nTo achieve their ends."},
+      {name: "Slytherin", img: Slytherin, info: "You belong in Slytherin,\n There you'll make your real friends,\nThose cunning folks use any means\nTo achieve their ends."},
       {name: "Ravenclaw", img: Ravenclaw, info: "You belong in wise old Ravenclaw,\nif you've a ready mind,\nWhere those of wit and learning,\nWill always find their kind"},
       {name: "Hufflepuff", img: Hufflepuff, info: "You belong in Hufflepuff,\nWhere they are just and loyal,\nThose patient Hufflepuffs are true\nAnd unafraid of toil"}
     ]
