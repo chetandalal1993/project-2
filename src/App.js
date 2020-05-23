d9c2bce82d79b76e815b6dcf9043a8ce0f4be9d8
YardenLawson <yardenlawson@gmail.com>
2020-05-23 16:20:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 // import { getAllData } from '../../lib/api'
-import './components/assets/fonts/HARRYP__.TTF'
 
 import './App.css'
 
