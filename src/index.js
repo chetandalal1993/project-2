7eda0f163b6b0fe7ffcaac775e86e8d1f9f5d0a9
YardenLawson <yardenlawson@gmail.com>
2020-05-23 13:32:56 +0100

    font

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import './styles/main.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../assets/fonts/HARRYP__.TTF'
+// import '../assets/fonts/HARRYP__.TTF'
 
 ReactDOM.render(
   <React.StrictMode>
