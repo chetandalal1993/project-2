d6280d795958cb3e868c54b07ebcbd1a2eebe917
YardenLawson <yardenlawson@gmail.com>
2020-05-23 15:26:37 +0100

    font

--- src/components/Common/Home.js
+++ src/components/Common/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import hogwarts from '../assets/images/hogwarts2.png'
-// import '../assets/fonts/HARRYP__.TTF'
-
 
 const Home = () => (
   <section className="hero is-fullheight-with-navbar is-warning">
