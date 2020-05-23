cf4a5bf49a6c953bd21a37ad7b55303e321f5c16
YardenLawson <yardenlawson@gmail.com>
2020-05-23 17:37:50 +0100

    update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseUrl = 'https://www.potterapi.com/v1'
-const apiKey = `?key=$2a$10$3LzDUjIX8kPXxHyKIsVbB${process.env.REACT_APP_HARRY_POTTER_KEY}`
+const apiKey = `?key=$2a$10$t6cqE4.0tqBOAAkoYBb3G${process.env.REACT_APP_HARRY_POTTER_KEY}`
 
 export const getAllData = () => {
   console.log(apiKey)
