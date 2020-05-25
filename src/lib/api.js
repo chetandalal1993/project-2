a22e4fcfcd493a8fefb32d41578ca760002a0a49
YardenLawson <yardenlawson@gmail.com>
2020-05-25 12:59:27 +0100

    update

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,7 @@ const baseUrl = 'https://www.potterapi.com/v1'
 const apiKey = `?key=$2a$10$t6cqE4.0tqBOAAkoYBb3G${process.env.REACT_APP_HARRY_POTTER_KEY}`
 
 export const getAllData = () => {
-  console.log(apiKey)
+  // console.log(apiKey)
   return axios.get(`${baseUrl}/houses${apiKey}`)
 }
 
