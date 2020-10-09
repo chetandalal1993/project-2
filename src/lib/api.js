651a1e68a67da251585980dc53d8ba36f5b6fa30
chetandalal <chetandalal1993@gmail.com>
2020-10-09 17:05:12 +0530

    asqs

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const baseUrl = 'https://www.potterapi.com/v1'
-const apiKey = `?key=$2a$10$t6cqE4.0tqBOAAkoYBb3G${process.env.REACT_APP_HARRY_POTTER_KEY}`
-
+// const apiKey = `?key=$2a$10$t6cqE4.0tqBOAAkoYBb3G${process.env.REACT_APP_HARRY_POTTER_KEY}`
+const apiKey = `?key=$2a$10$t6cqE4.0tqBOAAkoYBb3G.ZZ8B8aJ55eOn5wTyv.mfkWgHT7xJ75y`
 export const getAllData = () => {
   // console.log(apiKey)
   return axios.get(`${baseUrl}/houses${apiKey}`)
@@ -12,4 +12,3 @@ export const getAllData = () => {
 
 
 
-// const apiKey = `?key=$2a$10$t6cqE4.0tqBOAAkoYBb3G.ZZ8B8aJ55eOn5wTyv.mfkWgHT7xJ75y`
