528ecd3a206a6b017ccddaf4ab8f6e399a2f5e18
chetandalal <chetandalal1993@gmail.com>
2020-10-09 17:02:49 +0530

    Updates

---
+++ precache-manifest.a1772bff4a33125d8ab15bc38822fb7e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c5f2c33c65acf0e4719faf20d21a66",
+    "url": "/project-2/index.html"
+  },
+  {
+    "revision": "fd2eb07b642a53901c6e",
+    "url": "/project-2/static/css/main.b9a91eb8.chunk.css"
+  },
+  {
+    "revision": "ac0592c7287bbe54ec7c",
+    "url": "/project-2/static/js/2.840c5ada.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-2/static/js/2.840c5ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2eb07b642a53901c6e",
+    "url": "/project-2/static/js/main.e5e3e92d.chunk.js"
+  },
+  {
+    "revision": "d8c9c460d1cc1f602b5b",
+    "url": "/project-2/static/js/runtime-main.baa1b481.js"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/project-2/static/media/HARRYP__.181ef9a7.TTF"
+  },
+  {
+    "revision": "a94c545248cd395f5fd95a6b26f4370f",
+    "url": "/project-2/static/media/demiguise.a94c5452.png"
+  },
+  {
+    "revision": "af3e9ef54008c7923d4096985f114004",
+    "url": "/project-2/static/media/errorpixie.af3e9ef5.png"
+  },
+  {
+    "revision": "17cfdbfa3440ca20e4e487a98b0b5584",
+    "url": "/project-2/static/media/g2.17cfdbfa.png"
+  },
+  {
+    "revision": "569d46c4ec7029037b02b0df16109233",
+    "url": "/project-2/static/media/h2.569d46c4.png"
+  },
+  {
+    "revision": "f7acbc5a6c68fd29efbc3f860a65d8e3",
+    "url": "/project-2/static/media/hat.f7acbc5a.png"
+  },
+  {
+    "revision": "149f49989ddae12dc9279de623d50573",
+    "url": "/project-2/static/media/hogwarts2.149f4998.png"
+  },
+  {
+    "revision": "c71cb81fcd65b4f3b0bd824f26ef7e06",
+    "url": "/project-2/static/media/housescrest.c71cb81f.png"
+  },
+  {
+    "revision": "039b15c96ae884dcf8278f132e20c164",
+    "url": "/project-2/static/media/r2.039b15c9.png"
+  },
+  {
+    "revision": "e5bdac065824291bba18ae43c03b399c",
+    "url": "/project-2/static/media/s2.e5bdac06.png"
+  }
+]);
